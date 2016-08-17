241f6b36ea27769a0107e27f1dfc7743594b2736
Anastasiia Dragich <nastia@Myhomezys-MacBook-Air.local>
2016-08-17 12:33:54 +0200

    changes

--- place.php
+++ place.php
@@ -37,7 +37,7 @@
 	?>
 	<div class="grey-corner-left-top"></div>
 </section>
-<section class="scrollableContent place-section relative"  data-parallax='{"y" : -200, "from-scroll": 200, "distance": 1400, "smoothness": 10}'>
+<section class="scrollableContent place-section relative"  data-parallax='{"y" : -50, "from-scroll": 200, "distance": 1400, "smoothness": 10}'>
 	<div class="container">
 		<div class="col l12 m12 s12 placeWrap flex flex-center fadeInUp" data-parallax='{"y" : -200, "from-scroll": 200, "distance": 1400, "smoothness": 10}'>
 			<div class="col l7 s12 place-info animated fadeInUp wow" data-wow-delay="0.5s">
@@ -50,7 +50,7 @@
 				<img src="img/place/projections-venue_1.jpg" alt="Sala expo w Concordia Design">
 			</div>
 		</div>
-		<div class="col xl6 l12 m12 s12 placeWrap" data-parallax='{"y" : -200, "from-scroll": 300, "distance": 1600, "smoothness": 40}'>
+		<div class="col xl6 l12 m12 s12 placeWrap" data-parallax='{"y" : -100, "from-scroll": 300, "distance": 1600, "smoothness": 40}'>
 
 				<img src="img/place/projections-venue_2.jpg" alt="Sale warsztatowe w Concordia Design"
 					 class="animated fadeInUp wow" data-wow-delay="0.25s">
@@ -61,7 +61,7 @@
 						Spacious and easy to arrange – ideal for conferences, banquets, <br>concerts, team-building events, fashion shows, and many other occasions. </p>
 				</div>
 		</div>
-		<div class="col xl6 l12 m12 s12 placeWrap" data-parallax='{"y" : -200, "from-scroll": 300, "distance": 1400, "smoothness": 20}'>
+		<div class="col xl6 l12 m12 s12 placeWrap" data-parallax='{"y" : -100, "from-scroll": 300, "distance": 1400, "smoothness": 20}'>
 
 				<div class="l12 place-info animated fadeInUp wow" data-wow-delay="0.25s">
 					<p class="blockTitle big">228 m²</p>
