0ae03988fdf7a9817c088b4d62a3f3b51b53bbf3
Anastasiia Dragich <nastia@Myhomezys-MacBook-Air.local>
2016-08-18 12:51:25 +0200

    changes

--- partials/config.php
+++ partials/config.php
@@ -1,5 +1,7 @@
 <?php
 	//assign domain
 	$domain = "http://event-project.myhomezy.com/";
+    $feedback_email = 'anastasiia.dragich@gmail.com';
+    $feedback_subject = 'Application';
 	//$domain = "http://localhost/event/";
 ?>
