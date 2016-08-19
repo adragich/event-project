eae6407bb5f486c1f23d203b2374608b5cf1dbb3
Anastasiia Dragich <nastia@Myhomezys-MacBook-Air.local>
2016-08-19 10:08:39 +0200

    changes

--- actions/swiftmailer.php
+++ actions/swiftmailer.php
@@ -10,17 +10,17 @@ $mailer = Swift_Mailer::newInstance($transport);
     $message = Swift_Message::newInstance('Wonderful Subject')
         ->setFrom(array('john@doe.com' => 'John Doe'))
         ->setTo(array('anastasiia.dragich@gmail.com' => 'Nastia'));
-    $message->setBody('<html>
+    $message->setBody("<html>
     <body>
         <h2>Hi John!</h2><br><br>
         Johanna (johanna82) sent you a message.<br>
         <p>
-        	Hi John. Amazing picture... <a href="http://www.awesome.com/msg/12345/read/">login and read the full message</a>
+        	Hi John. Amazing picture... <a href='http://event-project.myhomezy.com/uploads/pdfurl-guide.pdf'>login and read the full message</a>
         </p>
         Best regards,<br>
         Photos4Lulz
     </body>
-</html>');
+</html>");
     if (!$mailer->send($message, $errors))
     {
         echo "Error:";
