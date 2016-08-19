5b08a0b479ba6f3bd1bc39bc0b7c8eecab0352cb
Anastasiia Dragich <nastia@Myhomezys-MacBook-Air.local>
2016-08-19 08:45:40 +0200

    changes

--- actions/swiftmailer.php
+++ actions/swiftmailer.php
@@ -1,2 +1,26 @@
 <?php
- echo 'here';
+    require_once '/home/www/event-project.myhomezy.com/web/vendor/swiftmailer/swiftmailer/lib/swift_required.php';
+    $transport = Swift_SmtpTransport::newInstance('smtp.example.org', 25)
+        ->setUsername('your username')
+        ->setPassword('your password');
+    $mailer = Swift_Mailer::newInstance($transport);
+    $message = Swift_Message::newInstance('Wonderful Subject')
+        ->setFrom(array('john@doe.com' => 'John Doe'))
+        ->setTo(array('anastasia.dragich@gmail.com' => 'Nastia'));
+    $message->setBody('<html>
+    <body>
+        <h2>Hi John!</h2><br><br>
+        Johanna (johanna82) sent you a message.<br>
+        <p>
+        	Hi John. Amazing picture... <a href="http://www.awesome.com/msg/12345/read/">login and read the full message</a>
+        </p>
+        Best regards,<br>
+        Photos4Lulz
+    </body>
+</html>');
+    if (!$mailer->send($message, $errors))
+    {
+        echo "Error:";
+        print_r($errors);
+    }
+?>
