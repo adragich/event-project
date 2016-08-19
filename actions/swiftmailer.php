b48df1abc640bd1bae91adbbe316371434954d8f
Anastasiia Dragich <nastia@Myhomezys-MacBook-Air.local>
2016-08-19 11:30:23 +0200

    changes

--- actions/swiftmailer.php
+++ actions/swiftmailer.php
@@ -17,7 +17,19 @@ else{
     $email = "user@aa.com";
 }
 
+if(isset($_POST['phone']) && !empty($_POST['phone'])){
+    $phone = $_POST['phone'];
+}
+if(isset($_POST['portfolio']) && !empty($_POST['portfolio'])){
+    $portfolio = $_POST['portfolio'];
+}
+if(isset($_POST['online-portfolio']) && !empty($_POST['online-portfolio'])){
+    $onlinePortfolio = 'Link for online portfolio'.$_POST['online-portfolio'];
+}
 
+if(isset($_POST['team']) && !empty($_POST['team'])){
+    $team = $_POST['team'];
+}
 
 // Create the Transport
 $transport = Swift_SendmailTransport::newInstance('/usr/sbin/sendmail -bs');
@@ -31,6 +43,7 @@ $mailer = Swift_Mailer::newInstance($transport);
     $message->setBody("<html>
                         <body>
                             <h2>Application!</h2><br><br>
+                            ".$name." sent an application.<br>
                             <p>
                                 Watch <a href='http://event-project.myhomezy.com/uploads/pdfurl-guide.pdf'>portfolio</a>
                             </p>
@@ -38,8 +51,7 @@ $mailer = Swift_Mailer::newInstance($transport);
                             Event-project
                         </body>
                     </html>", 'text/html')
-        ->attach(Swift_Attachment::fromPath('../uploads/pdfurl-guide.pdf'));
-
+        ->attach(Swift_Attachment::fromPath('../uploads/pdfurl-guide.pdf'));;
     if (!$mailer->send($message, $errors))
     {
         echo "Error:";
@@ -47,7 +59,5 @@ $mailer = Swift_Mailer::newInstance($transport);
     }
     else{
         echo 'Message has been sent';
-        header('Location: ' . $_SERVER['HTTP_REFERER'] . "#application");
     }
-
 ?>
