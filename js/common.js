4a2964131ed69b2ce485b5bc028ed7d92784549c
Anastasiia Dragich <nastia@Myhomezys-MacBook-Air.local>
2016-08-22 14:33:57 +0200

    changes

--- js/common.js
+++ js/common.js
@@ -25,7 +25,7 @@ function setPosition(checkAnim){
         right = left,
 
         //right = widthWrap - left - widthTemplate,
-        bottom = top,
+        bottom = top + offsetBottom,
        // bottom = heightWrap - top - heightTemplate,
        // bottomDefigned = bottom + heightTemplate - offsetBottom,
        // topDefigned = top + orangeSquare.height(),
