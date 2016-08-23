75e190efb852bbf19608673e61ed4ed4773a65e5
Anastasiia Dragich <nastia@Myhomezys-MacBook-Air.local>
2016-08-23 14:27:52 +0200

    changes

--- partials/text.php
+++ partials/text.php
@@ -183,8 +183,8 @@
 //			      main page
                 define("PROJECTS","Projekcje");
 
-                define("TITLE","Event dla projektantów");
-                define("NOTE","Przez projektantów");
+                define("TITLE","Konferencja graficzna");
+                define("NOTE","dla miłośników designu");
 
                 define("CITY","Poznań");
                 define("COMPANY","Concordia Design");
