0e9fb35a8fe9aa5112aae7a75fc9ca4399b60804
Anastasiia Dragich <nastia@Myhomezys-MacBook-Air.local>
2016-08-23 09:20:43 +0200

    changes

--- js/appear.js
+++ js/appear.js
@@ -10,18 +10,18 @@ $(function() {
 	// setTimeout('$(".big-grey-stripe").addClass(" animated fadeInDown")',  duration/2);
 	// setTimeout('$(".event-left-link").addClass(" animated fadeIn")',  0.75*duration);
 
-	setTimeout('$("#topPart").addClass(" animated fadeInDown")',  2.75*duration);
-	setTimeout('$("#middlePart").addClass(" animated fadeIn")',  2.5*duration);
-	setTimeout('$("#bottomPart").addClass(" animated fadeInUp")',  3*duration);
+	setTimeout('$("#topPart").addClass(" animated fadeInDown")',  0.75*duration);
+	setTimeout('$("#middlePart").addClass(" animated fadeIn")',  0.5*duration);
+	setTimeout('$("#bottomPart").addClass(" animated fadeInUp")',  duration);
 
-	setTimeout('$(".aside.right").addClass(" animated fadeInDown")',  2*duration/3);
+	// setTimeout('$(".aside.right").addClass(" animated fadeInDown")',  2*duration/3);
 
 
 	setTimeout('$(".mainTitle").addClass(" animated fadeInUp")',  duration);
-	setTimeout('$(".note").addClass(" animated fadeInUp")',  duration*1.5);
-	setTimeout('$(".date").addClass(" animated fadeInUp")',  duration*1.7);
-	setTimeout('$(".city").addClass(" animated fadeInUp")',  duration*1.9);
-	setTimeout('$(".company").addClass(" animated fadeInUp")',  duration*2.1);
-
-	setTimeout('$(".bottom-link").addClass(" animated fadeInRight")',  duration*1.25);
+	setTimeout('$(".note").addClass(" animated fadeInUp")',  duration*0.5);
+	// setTimeout('$(".date").addClass(" animated fadeInUp")',  duration*1.7);
+	// setTimeout('$(".city").addClass(" animated fadeInUp")',  duration*1.9);
+	// setTimeout('$(".company").addClass(" animated fadeInUp")',  duration*2.1);
+    //
+	// setTimeout('$(".bottom-link").addClass(" animated fadeInRight")',  duration*1.25);
 });
