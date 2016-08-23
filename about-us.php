c98addc263e954193a029f8843fe7109fa67d03d
Anastasiia Dragich <nastia@Myhomezys-MacBook-Air.local>
2016-08-23 12:25:55 +0200

    changes

--- about-us.php
+++ about-us.php
@@ -78,7 +78,7 @@
 	<section class="scrollableContent about relative">
 		<div class="container">
 			<div class="col xl6 l12 m12 s12 speakerWrap fadeInUp" data-parallax='{"y" : -200, "from-scroll": 200, "distance": 1400, "smoothness": 10}'>
-				<div class="col l9 s12 speaker animated fadeInUp wow" data-wow-delay="0.2s">
+				<div class="col l9 s12 speaker animated fadeInUp wow" data-wow-delay="0s">
 					<img src="img/speakers/Piotr-Swierkowski.jpg" alt="Piotr Swierkowski">
 					<div class="l12 speaker-info">
 						<p class="blockTitle">Piotr Swierkowski</p>
@@ -86,12 +86,12 @@
 						<p class="block-note">www.torpedov.com</p>
 					</div>
 				</div>
-				<div class="col m3 s12 stick-bottom animated fadeIn wow" data-wow-delay="0.2s">
+				<div class="col m3 s12 stick-bottom animated fadeIn wow" data-wow-delay="0s">
 					<a href="" class="learn-more-link yellow"><?php echo LEARN_MORE;?></a>
 				</div>
 			</div>
 			<div class="col xl6 l12 m12 s12 speakerWrap" data-parallax='{"y" : -250, "from-scroll": 300, "distance": 1600, "smoothness": 40}'>
-				<div class="col l9 s12 speaker animated fadeInUp wow" data-wow-delay="0.2s">
+				<div class="col l9 s12 speaker animated fadeInUp wow" data-wow-delay="0s">
 					<img src="img/speakers/Kuba-Enzowski.jpg" alt="Kuba Enzowski">
 					<div class="l12 speaker-info">
 						<p class="blockTitle">Kuba Enzowski</p>
@@ -99,12 +99,12 @@
 						<p class="block-note">behance.net/komunikat</p>
 					</div>
 				</div>
-				<div class="col m3 s12 stick-bottom animated fadeIn wow" data-wow-delay="0.2s">
+				<div class="col m3 s12 stick-bottom animated fadeIn wow" data-wow-delay="0s">
 					<a href="" class="learn-more-link yellow"><?php echo LEARN_MORE;?></a>
 				</div>
 			</div>
 			<div class="col xl6 l12 m12 s12 speakerWrap" data-parallax='{"y" : -300, "from-scroll": 300, "distance": 1400, "smoothness": 20}'>
-				<div class="col l9 s12 speaker animated fadeInUp wow" data-wow-delay="0.2s">
+				<div class="col l9 s12 speaker animated fadeInUp wow" data-wow-delay="0s">
 					<img src="img/speakers/Natalia-Zerko.jpg" alt="Natalia Żerko">
 					<div class="l12 speaker-info">
 						<p class="blockTitle">Natalia Żerko</p>
@@ -112,12 +112,12 @@
 						<p class="block-note">behance.net/komunikat</p>
 					</div>
 				</div>
-				<div class="col m3 s12 stick-bottom animated fadeIn wow" data-wow-delay="0.2s">
+				<div class="col m3 s12 stick-bottom animated fadeIn wow" data-wow-delay="0s">
 					<a href="" class="learn-more-link yellow"><?php echo LEARN_MORE;?></a>
 				</div>
 			</div>
 			<div class="col xl6 l12 m12 s12 speakerWrap" data-parallax='{"y" : -250, "from-scroll": 300, "distance": 1600, "smoothness": 10}'>
-				<div class="col l9 s12 speaker animated fadeInUp wow" data-wow-delay="0.2s">
+				<div class="col l9 s12 speaker animated fadeInUp wow" data-wow-delay="0s">
 					<img src="img/speakers/Ewelina-Dymek.jpg" alt="Ewelina Dymek">
 					<div class="l12 speaker-info">
 						<p class="blockTitle">Ewelina Dymek</p>
@@ -125,7 +125,7 @@
 						<p class="block-note">behance.net/edmk</p>
 					</div>
 				</div>
-				<div class="col m3 s12 stick-bottom animated fadeIn wow" data-wow-delay="0.2s">
+				<div class="col m3 s12 stick-bottom animated fadeIn wow" data-wow-delay="0s">
 					<a href="" class="learn-more-link yellow"><?php echo LEARN_MORE;?></a>
 				</div>
 			</div>
