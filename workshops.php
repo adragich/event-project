084907f878d9ec2fb2f0c5fc075e58508fdcc550
Anastasiia Dragich <nastia@Myhomezys-MacBook-Air.local>
2016-08-25 17:18:08 +0200

    changes

--- workshops.php
+++ workshops.php
@@ -92,7 +92,7 @@
 			</div>
 			<div class="workshop-popup-inner">
 				<figure class="pic">
-					<img src="" alt="">
+					<img src="img/workshops/projections-workshops-popup_1.jpg" alt="<?php echo WORKSHOPS_TITLE_1;?>">
 				</figure>
 			</div>
 		</div>
