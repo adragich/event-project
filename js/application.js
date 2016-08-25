4e162550db69b45ecb5fd8fabdcf9da91a6a159a
Anastasiia Dragich <nastia@Myhomezys-MacBook-Air.local>
2016-08-25 09:46:21 +0200

    changes

--- js/application.js
+++ js/application.js
@@ -96,6 +96,36 @@ $(document).ready(function(){
             e.preventDefault();
             label.addClass('required');
         }
+        else{
+
+                e.preventDefault();
+
+                var name = $( '[name = "name"]' ).val(),
+                    email = $( '[name = "email"]' ).val(),
+                    phone = $( '[name = "phone"]' ).val(),
+                    portfolio = $('#fileName').text(),
+                    onlinePortfolio = $( '[name = "address"]' ).val(),
+                    team = $('.chosen-team').text();
+
+                var data = {
+                    'name': name,
+                    'email': email,
+                    'phone': phone,
+                    'portfolio': portfolio,
+                    'online-portfolio': 'http://' + onlinePortfolio,
+                    'team': team
+                };
+
+                $.ajax({
+                    method: 'POST',
+                    url: '../actions/swiftmailer.php',
+                    data: data,
+                    dataType: 'json'
+                }).done(function(e){
+                    console.log(e);
+                    console.log('done');
+                });
+            }
 
     });
 
