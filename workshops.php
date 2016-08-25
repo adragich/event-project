cf3824aa89bdcb8cf23c5a9b452f6ea5a4953e55
Anastasiia Dragich <nastia@Myhomezys-MacBook-Air.local>
2016-08-25 15:24:26 +0200

    changes

--- workshops.php
+++ workshops.php
@@ -36,110 +36,13 @@
 			?>
 		</div>
 	</section>
-	<section class="scrollableContent relative program-section" id="workshopSection">
-		<div class="container program-wrap">
-			<table class="program-table">
-				<thead>
-				<tr>
-					<th colspan="2" class="program-date"><span>05</span> <?php echo MONTH;?> 2016 _</th>
-					<th>
-						<ul class="program-types flex">
-							<li class="flex"><span class="yellow-light"></span>
-								<?php echo PROGRAM_TYPE_1;?>
-							</li>
-							<li class="flex"><span class="blue-light"></span>
-								<?php echo PROGRAM_TYPE_2;?>
-							</li>
-						</ul>
-					</th>
-				</tr>
-				</thead>
-				<tbody>
-				<tr class="clear-string"></tr>
-				<tr class="offset-string"></tr>
-				<tr class="content-string no-button">
-					<td class="program-time">10:00</td>
-					<td class="left-align">
-						<p class="blockTitle">Grzegorz Róg</p>
-						<p class="block-content">Owner at eduweb.pl _</p>
-					</td>
-					<td class="left-align image yellow-gradient">
-						<img src="img/program/program_small_1.jpg" alt="Grzegorz Róg">
-						<span class="block-content">
-								<?php echo PROGRAM_DESCRIPTION_1;?>
-							</span>
-					</td>
-				</tr>
-				<tr class="offset-string"></tr>
-				<tr class="content-string no-button">
-					<td class="program-time">11:00</td>
-					<td class="left-align">
-						<p class="blockTitle">Martyna Wędzicka</p>
-						<p class="block-content">lesstudio.pl _</p>
-					</td>
-					<td class="left-align image yellow-gradient">
-						<img src="img/program/program_small_2.jpg" alt="Martyna Wędzicka">
-						<span class="block-content">
-								<?php echo PROGRAM_DESCRIPTION_2;?></span>
-					</td>
-				</tr>
-				<tr class="offset-string"></tr>
-				<tr class="content-string">
-					<td class="program-time">13:00</td>
-					<td class="left-align">
-						<p class="blockTitle">Patryk Hardziej</p>
-						<p class="block-content">negation studio _</p>
-					</td>
-					<td class="left-align image blue-light">
-						<img src="img/program/program_small_3.jpg" alt="Patryk Hardziej">
-						<span class="block-content">
-								<?php echo PROGRAM_DESCRIPTION_3;?></span>
-						<a href="#" class="workshop transition"><?php echo APPLY_WORKSHOP; ?></a>
-					</td>
-				</tr>
-				<tr class="offset-string"></tr>
-				<tr class="content-string no-button">
-					<td class="program-time">14:00</td>
-					<td class="left-align">
-						<p class="blockTitle">Hopa Studio</p>
-						<p class="block-content">hopastudio.com _</p>
-					</td>
-					<td class="left-align image yellow-gradient">
-						<img src="img/program/program_small_4.jpg" alt="Hopa Studio">
-						<span class="block-content">
-								<?php echo PROGRAM_DESCRIPTION_4;?></span>
-					</td>
-				</tr>
-				<tr class="offset-string"></tr>
-				<tr class="content-string">
-					<td class="program-time">16:00</td>
-					<td class="left-align">
-						<p class="blockTitle">Kriss Hermansson <br>Marcus Brown
-						</p>
-						<p class="block-content">resn.co.nz _</p>
-					</td>
-					<td class="left-align image blue-light">
-						<img src="img/program/program_small_5.jpg" alt="Kriss Hermansson Marcus Brown">
-						<span class="block-content">
-								<?php echo PROGRAM_DESCRIPTION_5;?></span>
-						<a href="#" class="workshop transition"><?php echo APPLY_WORKSHOP; ?></a>
-					</td>
-				</tr>
-				<tr class="offset-string"></tr>
-				</tbody>
-			</table>
-			<div class="program-post-info">
-				<div class="main flex flex-center padding-2x program">
-					<div class="inside">
-						<p class="mainTitle"><?php echo PROGRAM_TITLE_2;?></p>
-						<p class="note"><?php echo PROGRAM_NOTE_2;?></p>
-						<div id="middlePart" class="transition hide animated fadeIn"></div>
-					</div>
-				</div>
-				<div class="concordia-image">
-					<a href="#"><img src="img/program/program_concordia.jpg" alt="Concordia design"></a>
-				</div>
-			</div>
+	<section class="scrollableContent relative workshop-section" id="workshopSection">
+		<div class="container">
+			<p class="blockTitle">Na wybrane warsztaty możesz zapisać się kupując bilet na konferencję !
+				Warsztaty nie są dodatkowo płatne, lecz ilość miejsc jest ograniczona.</p>
+			<p class="block-content">Poniżej znajduje się lista warsztatów, które odbędą się podczas konferencji. 
+				Kliknij i dowiedz się więcej o każdym z nich.
+			</p>
 		</div>
 	</section>
 	<!--menu-->
