89d90a2d16386b214065a56b05cd6175ebfb9157
Anastasiia Dragich <nastia@Myhomezys-MacBook-Air.local>
2016-08-30 13:36:21 +0200

    changes

--- battles.php
+++ battles.php
@@ -229,7 +229,7 @@
 								<img src="img/members/projections-bitwy-member-3.jpg" alt="Team logo">
 							</div>
 							<p class="blockTitle">Łukasz Słotwiński</p>
-							<p class="block-content">Creative Director nc+</p>
+							<p class="block-content">Creative Director nc+ / Design Canal</p>
 						</div>
 					</div>
 					<div class="team col l4 m4 s12 animated wow fadeInDown" data-wow-delay="0.2s">
