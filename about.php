030a781156da65cc7be09154d15ce46135a060b1
Anastasiia Dragich <nastia@Myhomezys-MacBook-Air.local>
2016-09-05 15:19:55 +0200

    last changes

--- about.php
+++ about.php
@@ -135,7 +135,10 @@
 				<div class="workshop-popup-inner">
 					<p class="absolute owner">
 						<a href="//www.<?php echo constant(ABOUT_WEBSITE_.$i);?>"
-						   target="_blank"><span><?php echo constant(ABOUT_WEBSITE_.$i);?></span></a></p>
+						   target="_blank" class="relative">
+							<span class="innerHoverStripe absolute transition">
+									</span>
+							<span><?php echo constant(ABOUT_WEBSITE_.$i);?></span></a></p>
 					<p class="blockTitle modal"><?php echo constant(ABOUT_TITLE_.$i);?></p>
 				</div>
 			</div>
