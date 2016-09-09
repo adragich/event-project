3c5ca03be816d85acd166a27819bf55e2ad0dd63
Anastasiia Dragich <nastia@Myhomezys-MacBook-Air.local>
2016-09-09 16:55:24 +0200

    very last changes

--- battles.php
+++ battles.php
@@ -42,9 +42,30 @@
 				<p class="mainTitle battle-info small image-title animated wow fadeInDown" data-wow-delay="0s">
 					<?php echo BATTLES_CONTENT_TITLE; ?>
 				</p>
-				<div class="battlesPlan">
-					<?php for($i = 1; $i <= 6; $i++){?>
+				<div class="battlesPlan col s12 m6 l6">
+					<?php for($i = 1; $i <= 3; $i++){?>
 					<div class="col s12 m6 l6 battlesPlanBlock">
+						<img src="img/battles/battle-figure-<?php echo $i; ?>.png" alt="Battle plan">
+						<span class="digit">0<?php echo $i; ?>.</span>
+						<div class="content">
+							<p class="blockTitle battlePlanText "><?php echo constant(BATTLES_PLAN_.$i);?></p>
+							<p class="block-content battlePlanText ">
+								<a href="//.www<?php echo constant(BATTLES_PLAN_SUB_.$i);?>"
+								   target="_blank" class="relative">
+									<span class="innerHoverStripe absolute transition">
+									</span>
+									<span class="relative">
+										<?php echo constant(BATTLES_PLAN_SUB_.$i);?>
+									</span>
+								</a>
+							</p>
+						</div>
+					</div>
+					<?php } ?>
+				</div>
+				<div class="battlesPlan col s12 m6 l6">
+					<?php for($i = 3; $i <= 6; $i++){?>
+					<div class="col l12 battlesPlanBlock">
 						<img src="img/battles/battle-figure-<?php echo $i; ?>.png" alt="Battle plan">
 						<span class="digit">0<?php echo $i; ?>.</span>
 						<div class="content">
