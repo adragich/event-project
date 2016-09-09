9de325d8a89a4821b5db91e68c45a8ef1ae6b5ca
Anastasiia Dragich <nastia@Myhomezys-MacBook-Air.local>
2016-09-09 18:28:26 +0200

    very last changes

--- partials/config.php
+++ partials/config.php
@@ -6,7 +6,7 @@
     $ticketsLink = '#';
 
     $templatePath = '/templates/projekcje_bitwy_szablon.pdf';
-    $feedback_email = 'anastasiia.dragich@gmail.com';
+    $feedback_email = 'projekcjeconf@gmail.com';
     $feedback_subject = 'Application';
     $feedback_from = 'user@event-project.myhomezy.com';
     $feedback_username = 'Administrator';
@@ -16,7 +16,7 @@
     $facebookLink = "https://www.facebook.com/projekcje2016/";
     $twitterLink = "https://twitter.com/Projekcje";
 
-    $shareImage = "SHARE_IMAGE_PATH.MIME";
+    $shareImage = "grafika-do-share.png";
 	//$domain = "http://localhost/event/";
 
 ?>
