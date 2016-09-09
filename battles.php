c67daab84c25ebe64b62f2f46cb2303a51b1fbb3
Anastasiia Dragich <nastia@Myhomezys-MacBook-Air.local>
2016-09-09 16:53:07 +0200

    very last changes

--- battles.php
+++ battles.php
@@ -44,7 +44,7 @@
 				</p>
 				<div class="battlesPlan">
 					<?php for($i = 1; $i <= 6; $i++){?>
-					<div class="col s12 m6 l6 battlesPlan-block">
+					<div class="col s12 m6 l6 battlesPlanBlock">
 						<img src="img/battles/battle-figure-<?php echo $i; ?>.png" alt="Battle plan">
 						<span class="digit">0<?php echo $i; ?>.</span>
 						<div class="content">
