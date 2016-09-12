d7802f9fd3e933c650703ee411ffc6ca3523c687
Anastasiia Dragich <nastia@Myhomezys-MacBook-Air.local>
2016-09-12 11:18:52 +0200

    changes

--- actions/swiftmailer.php
+++ actions/swiftmailer.php
@@ -32,8 +32,7 @@
 
     if(isset($_POST['address']) && !empty($_POST['address'])){
         $onlinePortfolio = "<b>Adres online portfolio: </b><br>
-                            <a href='://".$_POST['address']."'>".$_POST['address']."
-                            </a>";
+                            <a href='://".$_POST['address']."'>".$_POST['address']."</a>";
     }
     else{
         $onlinePortfolio = '';
